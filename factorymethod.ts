3d7011b3386d02502f2abef07e5d9cfffdfb818f
AnitaDSM-155 <anahiperezdsm@gmail.com>
2026-08-25 15:52:24 -0300

    desarrollando de apoco el ejercicio 2 (factoryMethod)

--- factorymethod.ts
+++ factorymethod.ts
@@ -1,23 +1,54 @@
-interface EquipoFac {
-    nombre: string,
-    ram: number,
-    procesador: string
+abstract class TipoEquipo {
+    nombre: string;
+    constructor(nombre: string){
+        this.nombre = nombre
+    }
+    abstract detalles(): string;
 }
 
-class Notebook implements EquipoFac {
-
+class Notebook extends TipoEquipo {
+    ram: number;
+    procesador: string;
 
     constructor(nombre: string, ram: number, procesador: string){
-
+        // this.nombre = nombre;
+        super(nombre)
+        this.ram = ram;
+        this.procesador = procesador;
     }
-
+    detalles(): string {
+        return `Tipo: Notebook, Nombre: ${this.nombre}, RAM: ${this.ram}, Procesador: ${this.procesador}`;
+    }
 }
 
-class Desktop implements EquipoFac {
-
+class Desktop extends TipoEquipo {
+    ram: number;
+    procesador: string;
+    
+    constructor(nombre: string, ram: number, procesador: string){
+        super(nombre)
+        this.nombre = nombre;
+        this.ram = ram;
+        this.procesador = procesador;
+    }
+    detalles(): string {
+        return `Tipo: Desktop, Nombre: ${this.nombre}, RAM: ${this.ram}, Procesador: ${this.procesador}`
+    }
 }
 
-class Servidor implements EquipoFac {
+class Servidor extends TipoEquipo{
+    //nombre: string;
+    ram: number;
+    procesador: string;
+
+    constructor(nombre: string, ram: number, procesador: string){
+        super(nombre)
+        this.ram = ram;
+        this.procesador = procesador;
+    }
+    detalles(): string {
+        return `Tipo: Servidor, Nombre: ${this.nombre}, RAM: ${this.ram}, Procesador: ${this.procesador}`
+    }
 
 }
 
@@ -27,6 +58,11 @@ class EquipoFactory {
     }
 
     crearEquipo(){
-    
+        //tiene que retornar una instancia de la clase adecuada; notebook, desktop o servidor
     }
 }
+
+const factory = new EquipoFactory();
+//const Notebook = factory.crearEquipo("Notebook", "Dell XPS", "16GB", "i7");
+//console.log(Notebook.detalles());
+// Tipo: Notebook, Nombre: Dell XPS, RAM: 16GB, Procesador: i7
