8310fa231822e647cee16d4eb7474c38768e009c
Anahi Perez <anahiperesdsm@gmail.com>
2026-08-25 22:07:25 -0300

    ejercicio factorymethod terminado

--- factorymethod.ts
+++ factorymethod.ts
@@ -3,14 +3,14 @@ abstract class TipoEquipo {
     constructor(nombre: string){
         this.nombre = nombre
     }
-    abstract detalles(): string;
+    abstract detalles(): string; //no tiene codigo, cada clase hijo lo completa, esta obligado a usarlo
 }
 
-class Notebook extends TipoEquipo {
-    ram: number;
+class NotebookEquipo extends TipoEquipo { //tuve que cambiar el nombred e mi clase porque choca con la variable que tiene el profe
+    ram: string; //TENGIO QUE CAMBIAR EL TIPO DE RAM A STRING PORQ EN EL BLOQUE DE CODIGO QUE PASO EL PROFE TIENE "16GB" y lo que dice el profe tiene q ser :)
     procesador: string;
 
-    constructor(nombre: string, ram: number, procesador: string){
+    constructor(nombre: string, ram: string, procesador: string){
         // this.nombre = nombre;
         super(nombre)
         this.ram = ram;
@@ -22,12 +22,12 @@ class Notebook extends TipoEquipo {
 }
 
 class Desktop extends TipoEquipo {
-    ram: number;
+    ram: string;
     procesador: string;
     
-    constructor(nombre: string, ram: number, procesador: string){
+    constructor(nombre: string, ram: string, procesador: string){
         super(nombre)
-        this.nombre = nombre;
+        //this.nombre = nombre;
         this.ram = ram;
         this.procesador = procesador;
     }
@@ -38,10 +38,10 @@ class Desktop extends TipoEquipo {
 
 class Servidor extends TipoEquipo{
     //nombre: string;
-    ram: number;
+    ram: string;
     procesador: string;
 
-    constructor(nombre: string, ram: number, procesador: string){
+    constructor(nombre: string, ram: string, procesador: string){
         super(nombre)
         this.ram = ram;
         this.procesador = procesador;
@@ -57,12 +57,20 @@ class EquipoFactory {
     constructor(){
     }
 
-    crearEquipo(){
-        //tiene que retornar una instancia de la clase adecuada; notebook, desktop o servidor
+    crearEquipo(tipo: "Notebook" | "Desktop" | "Servidor", nombre: string, ram: string, procesador: string){
+        //tiene que retornar una instancia de la clase adecuada; notebook, desktop o servidor, usamos swith segun el tipo de caso se retorna uno en especifico
+        switch(tipo){
+            case "Notebook":
+                return new NotebookEquipo(nombre, ram, procesador) 
+            case "Desktop":
+                return new Desktop(nombre, ram, procesador)
+            case "Servidor":
+                return new Servidor(nombre, ram, procesador)
+        }
     }
 }
 
 const factory = new EquipoFactory();
-//const Notebook = factory.crearEquipo("Notebook", "Dell XPS", "16GB", "i7");
-//console.log(Notebook.detalles());
+const Notebook = factory.crearEquipo("Notebook", "Dell XPS", "16GB", "i7");
+console.log(Notebook.detalles());
 // Tipo: Notebook, Nombre: Dell XPS, RAM: 16GB, Procesador: i7
